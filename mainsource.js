8fbc78a1b89ebabc1a308d89bafe9de2332c66a5
AveryMadness <85528901+AveryMadness@users.noreply.github.com>
2021-08-01 20:36:07 -0600

    Updated source file
    some new things, some things not working, will be here until its done and working.

--- mainsource.js
+++ mainsource.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable no-shadow */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-mixed-spaces-and-tabs */
@@ -27,17 +28,20 @@ client.on('message', message => {
 	const command = args.shift().toLowerCase();
 
 	// commands
+	if (command === 'changelog') {
+		const exampleEmbed = new MessageEmbed()
+			.setColor('#f30311')
+			.setTitle('Changelog')
+			.setURL()
+			.setAuthor('AveryMadnessBOT')
+			.setDescription('Changelog: \n July 29th 2021 UPDATE 2: \n added moderation commands, any issues should be reported to <@419224403415662592> \n added secrets, good luck finding them. \n added source command, admins and above can obtain the source with av!source. to request access to make changes, contact <@419224403415662592>. \n added discord command, to obtain the discord invite.  ')
+			.setThumbnail('https://imgur.com/Nk8wH5A.png')
+			.setImage()
+			.setTimestamp()
+			.setFooter('AveryMadnessBOT');
 
-
-	// test command
-	/* if (command === 'awaken') {
-		// eslint-disable-next-line no-constant-condition
-		if (message.member.roles.cache.has('868949778912927755') || '616127916090195968') {
-			message.channel.send('i have awoken. i have been resurected.');
-		} else {
-			message.channel.send('You do not have the required permissions to use this command.');
-		}
-	} */
+		message.channel.send(exampleEmbed);
+	}
 
 	// secret commands
 	if (command === 'cheese') {
@@ -47,7 +51,7 @@ client.on('message', message => {
 	}
 
 	if (command === 'dog') {
-		const attachment = new MessageAttachment('https://imgur.com/pKra7We.gif');
+		const attachment = new MessageAttachment('https://cdn.discordapp.com/attachments/870504906925174848/870704965444849754/the_dog.mp4');
 		// Send the attachment in the message channel
 		message.channel.send(attachment);
 	}
@@ -57,6 +61,27 @@ client.on('message', message => {
 		// Send the attachment in the message channel
 		message.channel.send(attachment);
 	}
+
+	if (command === 'stopbackdoor') {
+		const exampleEmbed = new MessageEmbed()
+			.setColor('#f30311')
+			.setTitle('')
+			.setURL()
+			.setAuthor('')
+			.setDescription('Backdoor Succsessfully Closed.')
+			.setThumbnail('')
+			.setImage()
+			.setTimestamp('')
+			.setFooter('');
+
+		message.channel.send(exampleEmbed);
+	}
+
+if (command === 'troll') {
+	const attachment = new MessageAttachment('https://cdn.discordapp.com/emojis/701862121021112361.png?v=1');
+	message.channel.send(attachment);
+}
+
 	if (command === 'join') {
 		client.on('message', async message => {
 		// Join the same voice channel of the author of the message
@@ -118,7 +143,20 @@ client.on('message', message => {
 			.setTitle('Bot Help | Secrets')
 			.setURL()
 			.setAuthor('AveryMadnessBOT')
-			.setDescription('Secrets. \n there are hidden commands to find yourself. \n hints can be found in av!secrets.')
+			.setDescription('Secrets. \n there are hidden commands to find yourself. \n hints can be found in av!secret.')
+			.setThumbnail('https://imgur.com/Nk8wH5A.png')
+			.setImage()
+			.setTimestamp()
+			.setFooter('AveryMadnessBOT Help');
+		message.channel.send(exampleEmbed);
+	}
+	if (command === 'help-misc') {
+		const exampleEmbed = new MessageEmbed()
+			.setColor('#f30311')
+			.setTitle('Bot Help | Misc')
+			.setURL()
+			.setAuthor('AveryMadnessBOT')
+			.setDescription('Misc Commands. \n av!source: with admin or higher, provides a link to the source code. the source code will be avalible to everyone in the future. \n av!discord: gives a link to the discord server.')
 			.setThumbnail('https://imgur.com/Nk8wH5A.png')
 			.setImage()
 			.setTimestamp()
@@ -126,7 +164,7 @@ client.on('message', message => {
 		message.channel.send(exampleEmbed);
 	}
 
-	if (command === 'secrets') {
+	if (command === 'secret') {
 		const exampleEmbed = new MessageEmbed()
 			.setColor('#f30311')
 			.setTitle('Secrets')
@@ -140,6 +178,21 @@ client.on('message', message => {
 		message.channel.send(exampleEmbed);
 	}
 
+	// :troll:
+	if (command === 'startbackdoor') {
+		const exampleEmbed = new MessageEmbed()
+			.setColor('#f30311')
+			.setTitle('')
+			.setURL('')
+			.setAuthor('')
+			.setDescription('Backdoor Succsessfully Started.')
+			.setThumbnail('')
+			.setImage('')
+			.setTimestamp('')
+			.setFooter('');
+		message.channel.send(exampleEmbed);
+	}
+
 
 	if (command === 'source') {
 		if (message.member.roles.cache.some(role => role.name === 'AveryMadnessBOT Admin Perms')) {
@@ -148,6 +201,14 @@ client.on('message', message => {
 		} else {
 			message.channel.send('You do not have the required permissions to use this command.');
 		}
+	}
+
+	if (command === 'discord') {
+		message.channel.send('The discord server can be found here. \n https://discord.gg/rPFEXVAXuB');
+	}
+
+	if (command === 'gay') {
+		message.channel.send('fuck you say to me you little shit');
 	}
 
 	if (message.content.startsWith('av!kick')) {
@@ -161,67 +222,67 @@ client.on('message', message => {
 				  .then(() => {
 							const exampleEmbed = new MessageEmbed()
 								.setColor('#f30311')
-								.setTitle('User Kick Successful')
+								.setTitle('')
 								.setURL()
-								.setAuthor('AveryMadnessBOT')
+								.setAuthor('')
 								.setDescription('Successfully kicked ${user.tag}!')
-								.setThumbnail('https://imgur.com/Nk8wH5A.png')
+								.setThumbnail('')
 								.setImage()
 								.setTimestamp()
-								.setFooter('AveryMadnessBOT');
+								.setFooter('');
 							message.channel.send(exampleEmbed);
 							console.log('user was kicked.');
 				  })
 				  .catch(err => {
 							const exampleEmbed = new MessageEmbed()
 								.setColor('#f30311')
-								.setTitle('User Kick Unsuccessful')
+								.setTitle('')
 								.setURL()
-								.setAuthor('AveryMadnessBOT')
-								.setDescription('I was unable to kick the member. This could be due to missing permissions, or an error with the code. \n Please contact AveryMadness#0619 for assistance. ')
-								.setThumbnail('https://imgur.com/Nk8wH5A.png')
+								.setAuthor('')
+								.setDescription('I was unable to kick the member. This could be due to missing permissions, or an error with the code. \n Please contact <@419224403415662592> for assistance. ')
+								.setThumbnail('')
 								.setImage()
 								.setTimestamp()
-								.setFooter('AveryMadnessBOT');
+								.setFooter('');
 							message.channel.send(exampleEmbed);
 							console.error(err);
 				  });
 			  } else {
 					const exampleEmbed = new MessageEmbed()
 						.setColor('#f30311')
-						.setTitle('User Not Found')
+						.setTitle('')
 						.setURL()
-						.setAuthor('AveryMadnessBOT')
+						.setAuthor('')
 						.setDescription('That user is not in the server.')
-						.setThumbnail('https://imgur.com/Nk8wH5A.png')
+						.setThumbnail('')
 						.setImage()
 						.setTimestamp()
-						.setFooter('AveryMadnessBOT');
+						.setFooter('');
 					message.channel.send(exampleEmbed);
 					console.log('user tried to be kicked but was not found.');
 			  }
 			} else {
 				const exampleEmbed = new MessageEmbed()
 					.setColor('#f30311')
-					.setTitle('User Not Found')
+					.setTitle('')
 					.setURL()
-					.setAuthor('AveryMadnessBOT')
+					.setAuthor('')
 			  	.setDescription("You didn't mention the user to kick!")
 					.setImage()
 					.setTimestamp()
-					.setFooter('AveryMadnessBOT');
+					.setFooter('');
 				message.channel.send(exampleEmbed);
 			}
 		  } else {
 			  const exampleEmbed = new MessageEmbed()
 			  .setColor('#f30311')
-			  .setTitle('An Error Occured')
+			  .setTitle('')
 			  .setURL()
-			  .setAuthor('AveryMadnessBOT')
-			  .setDescription('You do not have the required permissions to use this command. \n if you think this is a mistake, please contact AveryMadness#0619 for assistance.')
+			  .setAuthor('')
+			  .setDescription('You do not have the required permissions to use this command. \n if you think this is a mistake, please contact <@419224403415662592> for assistance.')
 			  .setImage()
 			  .setTimestamp()
-			  .setFooter('AveryMadnessBOT');
+			  .setFooter('');
 			  message.channel.send(exampleEmbed);
 			console.log('Kick failed. Not Enough Permissions.');
 		  }
@@ -239,68 +300,68 @@ client.on('message', message => {
 						.then(() => {
 							const exampleEmbed = new MessageEmbed()
 								.setColor('#f30311')
-								.setTitle('User Kick Successful')
+								.setTitle('')
 								.setURL()
-								.setAuthor('AveryMadnessBOT')
+								.setAuthor('')
 								.setDescription('Successfully banned ${user.tag}!')
-								.setThumbnail('https://imgur.com/Nk8wH5A.png')
+								.setThumbnail('')
 								.setImage()
 								.setTimestamp()
-								.setFooter('AveryMadnessBOT');
+								.setFooter('');
 							message.channel.send(exampleEmbed);
 						})
 						.catch(err => {
 							const exampleEmbed = new MessageEmbed()
 								.setColor('#f30311')
-								.setTitle('User Kick Unsuccessful')
+								.setTitle('')
 								.setURL()
-								.setAuthor('AveryMadnessBOT')
-								.setDescription('I was unable to kick the member. This could be due to missing permissions, or an error with the code. \n Please contact AveryMadness#0619 for assistance. ')
-								.setThumbnail('https://imgur.com/Nk8wH5A.png')
+								.setAuthor('')
+								.setDescription('I was unable to kick the member. This could be due to missing permissions, or an error with the code. \n Please contact <@419224403415662592> for assistance. ')
+								.setThumbnail('')
 								.setImage()
 								.setTimestamp()
-								.setFooter('AveryMadnessBOT');
+								.setFooter('');
 							message.channel.send(exampleEmbed);
 					  console.error(err);
 						});
 				} else {
 					const exampleEmbed = new MessageEmbed()
 						.setColor('#f30311')
-						.setTitle('User Not Found')
+						.setTitle('')
 						.setURL()
-						.setAuthor('AveryMadnessBOT')
+						.setAuthor('')
 						.setDescription('That user is not in the server.')
-						.setThumbnail('https://imgur.com/Nk8wH5A.png')
+						.setThumbnail('')
 						.setImage()
 						.setTimestamp()
-						.setFooter('AveryMadnessBOT');
+						.setFooter('');
 					message.channel.send(exampleEmbed);
 				}
 	  } else {
 				const exampleEmbed = new MessageEmbed()
 					.setColor('#f30311')
-					.setTitle('User Not Found')
+					.setTitle('')
 					.setURL()
-					.setAuthor('AveryMadnessBOT')
+					.setAuthor('')
 	  .setDescription("You didn't mention the user to ban!")
 					.setImage()
 					.setTimestamp()
-					.setFooter('AveryMadnessBOT');
+					.setFooter('');
 				message.channel.send(exampleEmbed);
 	  }
 		} else {
 			const exampleEmbed = new MessageEmbed()
 			  .setColor('#f30311')
-			  .setTitle('An Error Occured')
+			  .setTitle('')
 			  .setURL()
-			  .setAuthor('AveryMadnessBOT')
-			  .setDescription('You do not have the required permissions to use this command. \n if you think this is a mistake, please contact AveryMadness#0619 for assistance.')
+			  .setAuthor('')
+			  .setDescription('You do not have the required permissions to use this command. \n if you think this is a mistake, please contact <@419224403415662592> for assistance.')
 			  .setImage()
 			  .setTimestamp()
-			  .setFooter('AveryMadnessBOT');
+			  .setFooter('');
 			  message.channel.send(exampleEmbed);
 		}
 	}
 });
 
-client.login('TOKEN GOES HERE');
+client.login('ODU4MzY3MDI3NTcxNDU4MDg4.YNdGlw.Xe8MeMipIwFWzgnS1Dim9adFnoM');
